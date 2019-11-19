a6946fcb0b03c91dca7536cd9b2e8bb21251dc54
harshil-nori12 <harshil.nori@gonuclei.com>
2019-11-19 14:30:23 +0530

    Refactored code

---
+++ src/codechef/Anumula.java
@@ -0,0 +1,53 @@
+package codechef;
+import java.util.*;
+
+public class Anumula {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer T = scanner.nextInt();
+        while (T > 0) {
+            Integer N = scanner.nextInt();
+            ArrayList<Integer> al = new ArrayList<>();
+            for (int i = 1; i <= Math.pow(2,N) ; i++) {
+                Integer num = scanner.nextInt();
+                al.add(num);
+            }
+            Collections.sort(al, new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o1 > o2 ? 1: -1;
+                }
+            });
+            List<Integer> sum = new ArrayList<>();
+            List<Integer> globalSum = new ArrayList<>();
+            List<Integer> values = new ArrayList<>();
+            for (int i = 1; i < al.size() && values.size() <= N; i++) {
+                if (!sum.contains(al.get(i))) {
+                    Integer size = globalSum.size();
+                    for (int j = 0; j < size; j++) {
+                        globalSum.add(globalSum.get(j) + al.get(i));
+                    }
+                    for (int j = 0; j <values.size() ; j++) {
+                        globalSum.add(values.get(j) + al.get(i));
+                    }
+                    values.add(al.get(i));
+                    sum.removeAll(sum);
+                    sum.addAll(globalSum);
+                } else {
+                    for (int j = 0; j < sum.size(); j++) {
+                        if (sum.get(j).equals(al.get(i))) {
+                            sum.remove(j);
+                            break;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < values.size() - 1; i++) {
+                System.out.print(values.get(i) + " ");
+            }
+            System.out.print(values.get(values.size() - 1));
+            System.out.println();
+            T --;
+        }
+    }
+}
--- src/codechef/IpcTrain.java
+++ src/codechef/IpcTrain.java
@@ -1,7 +1,6 @@
 package codechef;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class IpcTrain {
     public static void main(String[] args) {
@@ -10,88 +9,56 @@ public class IpcTrain {
         while (T > 0) {
             Integer N = sc.nextInt();
             Integer D = sc.nextInt();
-            Map<Integer, List<Integer>> map = new HashMap<>();
-            Map<Integer, Integer> sadMap = new HashMap<>();
+            List<Trainer> trainers = new ArrayList<>();
             while (N > 0) {
-                Integer d = sc.nextInt();
-                Integer t = sc.nextInt();
-                Integer s = sc.nextInt();
-                if (map.containsKey(d)) {
-                    List<Integer> list = map.get(d);
-                    list.add(s);
-                    map.put(d, list);
-                } else {
-                    List list = new ArrayList();
-                    list.add(s);
-                    map.put(d, list);
-                } if (sadMap.containsKey(s)) {
-                    sadMap.put(s, sadMap.get(s) + t);
-                } else {
-                    sadMap.put(s, t);
-                }
-                N --;
+                trainers.add(new Trainer(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+                N--;
             }
-            map = map.entrySet().stream()
-                    .sorted((a,b) -> (a.getKey() > b.getKey())? 1 : 0)
-                    .collect(Collectors.toMap(e -> e.getKey(), e-> e.getValue()));
-
-            List<Integer> heap = new ArrayList<>();
+            Collections.sort(trainers,new Comparator<Trainer>(){
+                public int compare(Trainer a,Trainer b){
+                    return Integer.compare(a.day, b.day);
+                }
+            });            PriorityQueue<Trainer> priorityQueue = new PriorityQueue<Trainer>(new Comparator<Trainer>() {
+                @Override
+                public int compare(Trainer o1, Trainer o2) {
+                    if (o1.sadness > o2.sadness)
+                        return -1;
+                    else if (o1.sadness < o2.sadness) {
+                        return 1;
+                    }
+                    return 0;
+                }
+            });
+            int j = 0;
             for (int i = 1; i <= D; i++) {
-                if (map.containsKey(i)) {
-                    for (Integer sadness: map.get(i)) {
-                        heap.add(sadness);
-                        siftUp(heap);
-                    }
-                } if (!heap.isEmpty()) {
-                    sadMap.put(heap.get(0), sadMap.get(heap.get(0)) - 1);
-                    if (sadMap.get(heap.get(0)) == 0) {
-                        Integer temp = heap.get(0);
-                        heap.set(0, heap.get(heap.size() - 1));
-                        heap.set(heap.size() - 1, temp);
-                        heap.remove(heap.size() - 1);
-                        siftDown(heap);
+                while (j< trainers.size() && trainers.get(j).day == i) {
+                        priorityQueue.offer(trainers.get(j));
+                        j++;
+                }
+                if (!priorityQueue.isEmpty()) {
+                    priorityQueue.peek().lectures --;
+                    if (priorityQueue.peek().lectures == 0) {
+                        priorityQueue.poll();
                     }
                 }
             }
-            Integer sum = 0;
-            for (int i = 0; i < heap.size(); i++) {
-                sum+=sadMap.get(heap.get(i)) * heap.get(i);
-            }
+            Integer sum = priorityQueue.stream().map(p -> p.sadness * p.lectures).reduce((e1, e2) -> e1+e2).orElse(0);
             System.out.println(sum);
             T --;
         }
-    }
 
-    static void siftUp(List<Integer> heap) {
-        Integer len = heap.size() - 1;
-        while (true) {
-            if (heap.get(len) > heap.get(len/2)) {
-                Integer temp = heap.get(len);
-                heap.set(len , heap.get(len/2));
-                heap.set(len/2, temp);
-                len = len/2;
-            } else {
-                break;
-            }
-        }
-    }
-
-    static void siftDown(List<Integer> heap) {
-        Integer len = 0;
-        while (true) {
-            if (2*len + 1 <= heap.size() - 1 && heap.get(2*len + 1) > heap.get(len)) {
-                Integer temp = heap.get(len);
-                heap.set(len, heap.get(2*len + 1));
-                heap.set(2*len + 1, temp);
-                len = 2*len + 1;
-            } else if (2*len + 2 <= heap.size() -1  && heap.get(2*len + 2) > heap.get(len)) {
-                Integer temp = heap.get(len);
-                heap.set(len, heap.get(2*len + 2));
-                heap.set(2*len + 2, temp);
-                len = 2* len + 2;
-            } else {
-                break;
-            }
-        }
     }
 }
+
+ class Trainer {
+     public Integer day;
+     public Integer lectures;
+     public Integer sadness;
+
+
+     Trainer(Integer day, Integer lectures, Integer sadness) {
+         this.day = day;
+         this.lectures = lectures;
+         this.sadness = sadness;
+     }
+ }
