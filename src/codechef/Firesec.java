1b0d121996b4187056796e864e831fefff8296a9
harshil-nori12 <harshil.nori@gonuclei.com>
2019-11-28 16:15:07 +0530

    Firesec solutions

--- src/codechef/Firesec.java
+++ src/codechef/Firesec.java
@@ -1,7 +1,6 @@
 package codechef;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Firesec {
     public static void main(String[] args) {
@@ -14,28 +13,36 @@ public class Firesec {
             Integer N = scanner.nextInt();
             Integer M = scanner.nextInt();
             List<Integer> list = new ArrayList<>();
+            List<Integer> size = new ArrayList<>();
             for (int j = 0; j < N; j++) {
                 list.add(j);
+                size.add(1);
             }
             for (int j = 0; j < M; j++) {
                 Integer n = scanner.nextInt();
                 Integer m = scanner.nextInt();
                 Integer root1 = findRoot(n-1, list);
                 Integer root2 = findRoot(m-1, list);
-                if (root1 > root2) {
-                    list.set(root1, root2);
-                } else {
+                if (root1.equals(root2)) {
+                }
+                else if (size.get(root1) > size.get(root2)) {
                     list.set(root2, root1);
+                    size.set(root1, size.get(root1) + size.get(root2));
+                } else {
+                    list.set(root1, root2);
+                    size.set(root2, size.get(root1) + size.get(root2));
                 }
             }
-            Map<Integer, Long> map =
-                    list.stream().collect(Collectors.groupingBy(Integer::intValue,
-                                    Collectors.mapping(Integer::intValue, Collectors.counting())));
-            System.out.print(map.keySet().size() + " ");
-            System.out.print(map.values().stream().reduce((k1,k2) -> (k1%1000000007)*(k2 % 1000000007)).get() % 1000000007);
-            if (i < T -1) {
-                System.out.println();
+            Long combinations = 1l;
+            Integer components = 0;
+            for (int j = 0; j < list.size(); j++) {
+                if (j == findRoot(j, list)) {
+                    components++;
+                    combinations = ((combinations%1000000007)*(size.get(j) % 1000000007))%1000000007;
+                }
             }
+            System.out.println(components + " " + combinations);
+
         }
     }
 
